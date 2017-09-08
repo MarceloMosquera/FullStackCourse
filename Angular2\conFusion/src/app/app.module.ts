00075879c1a3c29471b7b98607627b2c2580ad59
Marcelo Mosquera <marmosquera@gmail.com>
2017-09-08 18:16:01 -0300

    Template Forms Part 1

--- Angular2\conFusion/src/app/app.module.ts
+++ Angular2\conFusion/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent, PizzaPartyComponent, DialogOverviewExampleDialog } from './app.component';
 
@@ -20,21 +21,23 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaPartyComponent, 
-    DialogOverviewExampleDialog, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent, HomeComponent, AboutComponent, ContactComponent
+    DialogOverviewExampleDialog, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent, HomeComponent, AboutComponent, ContactComponent, LoginComponent
   ],
   imports: [
     BrowserModule,    
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  entryComponents: [DialogOverviewExampleDialog, PizzaPartyComponent],
+  entryComponents: [LoginComponent, PizzaPartyComponent],
   providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
