6f99b59aefeb8ed0a90d553930ffc0acbfd7926e
Marcelo Mosquera <marmosquera@gmail.com>
2017-09-13 23:07:05 -0300

    Animations Part 2

--- Angular2\conFusion/src/app/about/about.component.ts
+++ Angular2\conFusion/src/app/about/about.component.ts
@@ -3,11 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
 
+import { expand } from '../animations/app.animation';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
--- Angular2\conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Angular2\conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -10,22 +9,15 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/Comment';
 
+import { visibility, expand } from '../animations/app.animation';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(),
+    expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
