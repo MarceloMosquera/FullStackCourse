4dd088c3007cd770de46e491c7221a7d4875df1e
Marcelo Mosquera <marmosquera@gmail.com>
2017-09-25 19:28:02 -0300

    FAB Favotites

--- Ionic\conFusion/src/app/app.module.ts
+++ Ionic\conFusion/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
 import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-httpmsg';
+import { FavoriteProvider } from '../providers/favorite/favorite';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,8 @@ import { ProcessHttpmsgProvider } from '../providers/process-httpmsg/process-htt
     LeaderProvider,
     PromotionProvider,
     ProcessHttpmsgProvider,
-    { provide: 'BaseURL', useValue: baseURL }
+    { provide: 'BaseURL', useValue: baseURL },
+    FavoriteProvider
   ]
 })
 export class AppModule {}
