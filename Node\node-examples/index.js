a3971807983404fef3fe1c1511d78f77ab7246ac
Marcelo Mosquera <marmosquera@gmail.com>
2017-10-20 21:16:45 -0300

    simple example

---
+++ Node\node-examples/index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("solving rectangle");
+    if (l <= 0 || b <= 0) {
+        console.log("parametro menor que 0");
+    }
+    else {
+        console.log("area:" + rect.area(l,b));
+        console.log("perim:" + rect.perimeter(l,b));
+    }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
