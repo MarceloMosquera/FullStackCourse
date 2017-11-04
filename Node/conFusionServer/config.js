9e8c61790123386e54683e55f501c1be15d2baf2
Marcelo Mosquera <marmosquera@gmail.com>
2017-11-04 19:59:19 -0300

    Passport Facebook

--- Node/conFusionServer/config.js
+++ Node/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1965452963729735',
+        clientSecret: '997dd62d357131792b6dfa73c9d075c7'
+    }
 }
